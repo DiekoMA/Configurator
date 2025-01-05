522d4cce0dea1e9a6ec8b4a0123c1753bc4bc6aa
DiekoMA <dj_blueewastaken@outlook.com>
2025-01-06 00:17:50 +0100

    - UI Changes to show configs for each app

--- Configurator/MainWindowViewModel.cs
+++ Configurator/MainWindowViewModel.cs
@@ -1,14 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Configurator.Models;
+using Microsoft.Win32;
 using WGetNET;
 
 namespace Configurator;
 
 public class MainWindowViewModel : ObservableObject
 {
-    private List<WinGetPackage> _packages;
+    private InstalledApplication _selectedApplication;
 
-    public List<WinGetPackage> Packages
+    public InstalledApplication SelectedApplication
+    {
+        get => _selectedApplication;
+        set
+        {
+            _selectedApplication = value;
+            SetProperty(ref _selectedApplication, value);   
+            OnPropertyChanged();
+        }
+    }
+    private List<InstalledApplication> _packages;
+
+    public List<InstalledApplication> Packages
     {
         get => _packages;
         set
@@ -23,16 +41,54 @@ public class MainWindowViewModel : ObservableObject
         WinGetPackageManager pm = new WinGetPackageManager();
         if (wget.IsInstalled)
         {
-            Packages = pm.GetInstalledPackages();
-            foreach (var package in Packages)
-            {
-                
-            }
+            Packages = GetInstalledApplications();
         }
     }
 
-    public void GetInstalledApplications()
+    public static List<InstalledApplication> GetInstalledApplications()
     {
-        
+        var installedApplications = new List<InstalledApplication>();
+
+        string[] registryKeys =
+        {
+            @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+            @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+        };
+
+        foreach (var keyPath in registryKeys)
+        {
+            using RegistryKey key = Registry.LocalMachine.OpenSubKey(keyPath);
+            if (key != null)
+            {
+                foreach (var subKeys in key.GetSubKeyNames())
+                {
+                    using var subKey = key.OpenSubKey(subKeys);
+                    if (subKey == null) continue;
+                            
+                    var displayName = subKey.GetValue("DisplayName") as string;
+                    var displayIcoPath = subKey.GetValue("DisplayIcon") as string;
+                    var publisher = subKey.GetValue("Publisher") as string;
+                    var installLocation = subKey.GetValue("InstallLocation") as string;
+                            
+                    var guidRegex = new Regex("/^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$/gm");
+                    if (!string.IsNullOrEmpty(displayName))
+                    {
+                        installedApplications.Add(new InstalledApplication()
+                        {
+                            DisplayName = displayName ?? "Unknown",
+                            InstalLocation = installLocation ?? "Unknown",
+                            Publisher = publisher ?? "Unknown",
+                            //DisplayIcon = LoadFromResource(new Uri(displayIcoPath) ?? null)
+                        });
+                    }
+                }
+            }
+        }
+        return installedApplications;
+    }
+    
+    public static Bitmap LoadFromResource(Uri resourceUri)
+    {
+        return new Bitmap(AssetLoader.Open(resourceUri));
     }
 }
--- Configurator/Models/InstalledApplication.cs
+++ Configurator/Models/InstalledApplication.cs
@@ -1,9 +1,11 @@
-﻿namespace Configurator.Models;
+﻿using Avalonia.Media.Imaging;
+
+namespace Configurator.Models;
 
 public class InstalledApplication
 {
     public string DisplayName { get; set; }
-    public string DisplayIcon { get; set; }
+    public Bitmap DisplayIcon { get; set; }
     public string InstalLocation { get; set; }
     public string Publisher { get; set; }
 }
